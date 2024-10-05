b4606ca257349439cff98fa22d5a3f6f22457a40
Muhammad Mardinasyah <mardiansyahm002@gmail.com>
2024-10-05 14:59:17 +0700

    up

--- app/src/main/java/com/example/dicodingevent/ui/detail_event/DetailActivity.kt
+++ app/src/main/java/com/example/dicodingevent/ui/detail_event/DetailActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import androidx.navigation.navArgs
 import com.bumptech.glide.Glide
 import com.example.dicodingevent.databinding.ActivityDetailBinding
@@ -17,22 +18,33 @@ class DetailActivity : AppCompatActivity() {
     val args: DetailActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityDetailBinding.inflate(layoutInflater)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val id = args.idEvent
         viewModel.getDetailEvent(id)
 
         setContentView(binding.root)
 
         viewModel.detailEvent.observe(this) {
+            supportActionBar?.title = it.event?.name
+
             binding.tvOwnerDetailEvent.text = "Diselenggarakan oleh ${it.event?.ownerName}"
             binding.tvTitleDetailEvent.text = it.event?.name
             binding.tvQuotaDetailEvent.text = "Kuota: ${it.event?.quota}"
+            binding.tvBeginTimeDetailEvent.text = "Dimulai pada: ${it.event?.beginTime}"
+            binding.tvDescDetailEvent.text =
+                HtmlCompat.fromHtml(it.event?.description.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
             Glide.with(this)
                 .load(it.event?.mediaCover)
                 .into(binding.ivCoverDetailEvent)
         }
+
 
         viewModel.loading.observe(this) {
             if (it) {
